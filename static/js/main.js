642b8ed83fbf0fdf6f9f0cd7a9175c11b02e8695
GA-Eduardo <101217685+GA-Eduardo@users.noreply.github.com>
2023-12-21 20:36:12 +0100

    fdhdfh

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,17 @@
-
+$('img.lazyload').lazyload();
 
 function publicacion(source) {
-  
   $('#publicacion').attr('src', source);
   $('#publicacion').css('overflow', 'hidden');
   $('#publicacion').show();
   $('#publicacion').css('width', '100%');
   $('#publicacion').css('height', '2000');
   $('#publicacion').css('margin-top', 200);
-  let reducir=200
+  let reducir = 200;
   $('html, body').animate(
-     {
-       scrollTop: $('#publicacion').offset().top-reducir,
-     },
-     1000,
-   );
+    {
+      scrollTop: $('#publicacion').offset().top - reducir,
+    },
+    1000,
+  );
 }
