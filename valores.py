8fbab7235a7c0dcb90e75d968945a141b46c855e
IBM-SkillBuild <136986725+IBM-SkillBuild@users.noreply.github.com>
2023-12-23 08:37:50 +0100

    sfasdfs

--- valores.py
+++ valores.py
@@ -2,11 +2,17 @@
 
 class Valores():
   footer=False
-  MYSQL_HOST = "db4free.net"
+  #MYSQL_HOST = "db4free.net"
+  #MYSQL_PORT = 3306
+  #MYSQL_USER = "blog_edu_user"
+  #MYSQL_PASSWORD = "1.6180339"
+  #MYSQL_DB = "blog_edu"
+  
+  MYSQL_HOST = "sql8.freemysqlhosting.net"
   MYSQL_PORT = 3306
-  MYSQL_USER = "blog_edu_user"
-  MYSQL_PASSWORD = "1.6180339"
-  MYSQL_DB = "blog_edu"
+  MYSQL_USER = "sql8672421"
+  MYSQL_PASSWORD = "q7QAai4Eze"
+  MYSQL_DB = "sql8672421"
   URL="https://blog-edu-tech.koyeb.app"
 
 
