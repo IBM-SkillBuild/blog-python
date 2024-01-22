aff1c3ea965e475eae0a448fddecf08205db4dd8
GA-Eduardo <101217685+GA-Eduardo@users.noreply.github.com>
2024-01-22 10:08:10 +0100

    aaaa

--- config.py
+++ config.py
@@ -5,25 +5,7 @@ class ConfigDev():
   UPLOAD_FOLDER = 'static/uploads'
   UPLOAD_POST = 'static/posts'
   SECRET_KEY="MyApp"
-  MYSQL_HOST = "db4free.net"
-  MYSQL_PORT=3306
-  MYSQL_USER = "blog_edu_user"
-  MYSQL_PASSWORD = "1.6180339"
-  MYSQL_DB = "blog_edu"
-  
-  
-# con BBDD Prueba
-class ConfigDevClever():
-  DEBUG=True
-  TESTING=True
-  SECRET_KEY="MyApp"
-  UPLOAD_FOLDER = 'static/uploads'
-  UPLOAD_POST = 'static/posts'
-  MYSQL_HOST = "db4free.net"
-  MYSQL_PORT = 3306
-  MYSQL_USER = "blog_edu_user"
-  MYSQL_PASSWORD = "1.6180339"
-  MYSQL_DB = "blog_edu"
+    
  
   
     
@@ -34,11 +16,7 @@ class ConfigPro():
   SECRET_KEY="MyApp"
   UPLOAD_FOLDER = 'static/uploads'
   UPLOAD_POST = 'static/posts'
-  MYSQL_HOST = "db4free.net"
-  MYSQL_PORT = 3306
-  MYSQL_USER = "blog_edu_user"
-  MYSQL_PASSWORD = "1.6180339"
-  MYSQL_DB = "blog_edu"
+  
  
   
   
