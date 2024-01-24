015765be8c9bba22b9fa637fccb210e9c3cbaf31
GA-Eduardo <101217685+GA-Eduardo@users.noreply.github.com>
2024-01-24 12:53:22 +0100

    fddfh

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,9 @@
-
-$(document).ready(function() {
-  $("p").click(function(event) {
+const openMobileMenuBtn = document.querySelector('.device-menu');
+const headerMenu = document.querySelector('.header-nav');
+ 
+$(document).ready(function () {
+   $('.device-menu').css('display', 'none');
+  $('p').click(function (event) {
     let text = document.getElementById('url-clip').innerHTML;
     const copyContent = async () => {
       try {
@@ -9,8 +12,7 @@ $(document).ready(function() {
       } catch (err) {
         console.error('Failed to copy: ', err);
       }
-    }
-   
+    };
   });
 });
 
@@ -38,12 +40,7 @@ function publicacion(source) {
     },
     1000,
   );
-  
 }
-
-
-const openMobileMenuBtn = document.querySelector('.device-menu');
-const headerMenu = document.querySelector('.header-nav');
 
 openMobileMenuBtn.addEventListener('click', () => {
   if (openMobileMenuBtn.classList.contains('open')) {
@@ -55,24 +52,24 @@ openMobileMenuBtn.addEventListener('click', () => {
   }
 });
 
- 
-
 function mostrar_menu() {
- 
-   if (openMobileMenuBtn.classList.contains('open')) {
-     openMobileMenuBtn.classList.remove('open');
-     headerMenu.classList.remove('active');
-   } else {
-     headerMenu.classList.add('active');
-     openMobileMenuBtn.classList.add('open');
-   }
- }
+  
+  if (openMobileMenuBtn.classList.contains('open')) {
+    openMobileMenuBtn.classList.remove('open');
+    headerMenu.classList.remove('active');
+  } else {
+    headerMenu.classList.add('active');
+    openMobileMenuBtn.classList.add('open');
+  }
+}
 
 $(window).scroll(function () {
   if ($(this).scrollTop() > 300) {
     $('a.scroll-top').fadeIn('slow');
+     $('.device-menu').css('display', 'block');
   } else {
     $('a.scroll-top').fadeOut('slow');
+   
   }
 });
 $('a.scroll-top').click(function (event) {
@@ -81,19 +78,23 @@ $('a.scroll-top').click(function (event) {
 });
 
 document.querySelectorAll('.my-lightbox-toggle').forEach((el) =>
-  
-   el.addEventListener('click', (e) => {
-     e.preventDefault();
-     const lightbox = new Lightbox(el, options);
-     lightbox.show();
-   }),
-
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    const lightbox = new Lightbox(el, options);
+    lightbox.show();
+  }),
 );
- 
-
 
 function resizeIframe(obj) {
- 
-   obj.style.height =
-     obj.contentWindow.document.documentElement.scrollHeight + 135+'px';
- }
+  obj.style.height =
+    obj.contentWindow.document.documentElement.scrollHeight + 135 + 'px';
+}
+
+$(window).resize(function () {
+  if (screen.width < 758) {
+    $('.device-menu').css('display', 'block');
+  } 
+  else {
+    $('.device-menu').css('display', 'none');
+  }
+});
