af4069a09b15f63ab390f4272a10c071805117a9
GA-Eduardo <101217685+GA-Eduardo@users.noreply.github.com>
2024-02-02 13:21:50 +0100

    fdf

--- static/js/categorias_sitio.js
+++ static/js/categorias_sitio.js
@@ -1,6 +1,8 @@
 // javascript para el comportamiento del aside
 $(document).ready(function () {
- 
+   $('.aside-ctrl--reset').css({
+     witdh: '0%',
+   });
  // en el caso de pusar el boton flecha para abrir aside
   $("#aside-ctrl").click(function () {
     window.scrollTo(0, 0);
