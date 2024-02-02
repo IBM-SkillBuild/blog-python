860dc405eef8f49edc9423de1a7e2adf031a8e54
GA-Eduardo <101217685+GA-Eduardo@users.noreply.github.com>
2024-02-02 13:09:52 +0100

    kkkk

--- static/js/categorias_sitio.js
+++ static/js/categorias_sitio.js
@@ -1,6 +1,7 @@
-$( document ).ready(function() {
+// javascript para el comportamiento del aside
+$(document).ready(function () {
  
- 
+ // en el caso de pusar el boton flecha para abrir aside
   $("#aside-ctrl").click(function () {
     window.scrollTo(0, 0);
     if ($('#aside-ctrl').is(':checked')) {
@@ -8,20 +9,25 @@ $( document ).ready(function() {
           overflow: 'hidden',
           height: '100%',
         });
+      // aqui se quita el ancho del aside para cuando esté oculto
+      // es para evitar gestos del movil que puden enseñarlo
+      // a pesar de estar oculto
         $('.aside-ctrl--reset').css({
           witdh: '0%',
         });
-      
+      // se llamar a funcion ajax (python) peticion de datos
+      // e inyeccion de codigo para motrar contenido del aside
       cargar_aside_tags();
       $('html, body').css({
         overflow: 'auto',
         height: 'auto',
       });
-       
- 
-           
+        
     
-       
+      // este caso es para eliminar temporalmente el tamaño del aside
+      // ya que al estar oculto no precisa ancho alguno en css
+      // esto mejora la experiencia en el comportamiento de moviles
+      // ya que estos hacen gestos y podrian mostralo vacio.
     } else {
         $('.aside-ctrl--reset').css({
           witdh: '0%',
@@ -48,11 +54,13 @@ function cargar_aside_tags() {
     cache: false,
     success: function (result) {
       console.log(result)
+      // inyectamos pieza codigo en aside
       $('#aside-contenido').html('');
       $('#aside-contenido').append(result.htmlresponse);
+      // nos aseguramos del ancho del aside para ver resultados
        $('aside-ctrl--reset').css({
          overflow: 'hidden',
-         with: '0%',
+         witdh: '100%',
        });
        
       
